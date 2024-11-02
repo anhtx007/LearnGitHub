9903d9bbc39f506ea877d2cd2b58cec26cbfcf8c
aitx7270 <aitx7270@co-well.com.vn>
2024-11-02 23:56:08 +0700

    commt comment

--- app/src/main/java/com/example/learngithub/MainActivity.java
+++ app/src/main/java/com/example/learngithub/MainActivity.java
@@ -20,8 +20,20 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        // commit
-        // anh
-        // commit 1 cai
+        /**
+         * +++ git checkout -b dang-ky:
+         * - git checkout -b là để tạo nhánh mới và checkout qua nhánh mới luôn
+         * thay vì 2 thao tác là phải tạo rồi mwois checkout
+         * +++ git add . : để lưu tất cả các thay đổi vào stagging area(vùng chuẩn bị commit)
+         * +++ git commit -m "mô tả commit": -m là để viết mô tả commit ở trong cặp "" luôn
+         * nếu không có -m mà chỉ viết git commit thì git sẽ mở hộp thoại nhập mô tả commit
+         * vì git bắt buộc ng dùng nhập mô tả commit
+         * +++ git push-u origin dang-ky:
+         * '-u' : điều này sẽ khiến git ghi nhớ rằng nhánh ở local liên kết chặn với nhánh
+         * ở trên git(repo từ xa)
+         * - origin: ở đây là origin là tên, hiểu đơn giản thì origin giống như 1 Folder remote
+         * trên git, trong đó có các file là các nhánh, như nhánh main, nhánh phụ là các chức năng
+         *
+         */
     }
 }
